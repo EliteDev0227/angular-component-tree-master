4b18d802a6d117ebca9a88312cffe637b26630e6
Adam Klein <adam@500tech.com>
2016-07-13 14:00:53 +0300

    Trying to fix IE

--- lib/angular2-tree-component.ts
+++ lib/angular2-tree-component.ts
@@ -2,6 +2,7 @@ import { ITreeOptions } from './models/tree-options.model';
 import { TreeModel } from './models/tree.model';
 import { TreeNode } from './models/tree-node.model';
 import { TreeComponent } from './components/tree.component';
+import './polyfills';
 
 export {
   TreeModel,
---
+++ lib/polyfills.ts
@@ -0,0 +1,5 @@
+import 'element-closest';
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'ts-helpers';
