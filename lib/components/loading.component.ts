3d32c3da78d85f3b5c541598398b409373ff3cf9
Isaac Mann <isaacplmann@gmail.com>
2016-09-08 16:02:34 -0400

    Fix typo in loadingTemplate

--- lib/components/loading.component.ts
+++ lib/components/loading.component.ts
@@ -4,7 +4,7 @@ import { TreeModel } from '../models/tree.model';
 @Component({
   selector: 'LoadingComponent',
   template: `<span *ngIf="!loadingTemplate">loading...</span>
-  <template [ngTemplateOutlet]="loadingTemplate""></template>`,
+  <template [ngTemplateOutlet]="loadingTemplate"></template>`,
 })
 export class LoadingComponent {
   @Input() loadingTemplate: TemplateRef<any>;
