3cb254b077c734f88dae7016316d2ce789082d79
Adam Klein <adam@500tech.com>
2017-06-10 13:22:18 +0300

    Removed unwanted code

--- example/cli/src/app/app.component.ts
+++ example/cli/src/app/app.component.ts
@@ -8,9 +8,3 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 }
-
-@observer class TodoView extends React.Component {
-  render() {
-    return <div>{this.props.store.filteredItems}</div>
-  }
-}
