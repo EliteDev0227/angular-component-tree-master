db79d74bbac1b5d4f3c21f2bd8c258c2ea813010
Adam Klein <adam@500tech.com>
2017-07-18 09:07:30 +0300

    Added empty tree test

---
+++ e2e/empty.spec.ts
@@ -0,0 +1,34 @@
+import { browser, element, by, $ } from 'protractor';
+import { TreeDriver } from './helpers/tree.driver';
+
+describe('Empty Tree', () => {
+  beforeEach(() => {
+    browser.get('http://localhost:4200/#/empty');
+  });
+
+  const testEmptyTree = (treeId) => {
+    beforeEach(() => {
+      this.tree = new TreeDriver(treeId);
+    });
+
+    it('should show the tree', () => {
+      expect(this.tree.isPresent()).toBe(true);
+    });
+
+    it('should have 0 nodes', () => {
+      expect(this.tree.getNodes().count()).toEqual(0);
+    });
+
+    it('should load nodes into the tree', () => {
+      $('button').click();
+      expect(this.tree.getNodes().count()).toEqual(1);
+    });
+  };
+
+  describe('nodes = null', () => {
+    testEmptyTree('#tree1');
+  });
+  describe('nodes = []', () => {
+    testEmptyTree('#tree2');
+  });
+});
--- example/cli/src/app/empty/empty.component.ts
+++ example/cli/src/app/empty/empty.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empty',
   template: `
-    <tree-root #tree1 class="tree1" [focused]="true" [nodes]="nodes"></tree-root>
-    <tree-root #tree2 class="tree2" [focused]="true" [nodes]="nodes2"></tree-root>
-    <button (click)="do(tree1)">do</button>
-    <button (click)="do(tree2)">do2</button>
+    <tree-root #tree1 id="tree1" [focused]="true" [nodes]="nodes"></tree-root>
+    <tree-root #tree2 id="tree2" [focused]="true" [nodes]="nodes2"></tree-root>
+    <button (click)="loadNodes()">loadNodes 1</button>
   `,
   styles: []
 })
@@ -14,7 +13,8 @@ export class EmptyComponent {
   nodes = [];
   nodes2 = null;
 
-  do(tree) {
-    console.log(tree.treeModel.getNodeById(10));
+  loadNodes() {
+    this.nodes = [{ name: 'node' }];
+    this.nodes2 = [{ name: 'node' }];
   }
 }
