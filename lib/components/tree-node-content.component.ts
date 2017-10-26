a2527e60b5a0edd08b0b8caa7ea8f078aa2de5d0
vltr <istiti@users.noreply.github.com>
2017-10-26 08:44:58 +0200

    Update tree-node-content.component.ts
    Deprecate ngOutletContext. Use ngTemplateOutletContext instead.

--- lib/components/tree-node-content.component.ts
+++ lib/components/tree-node-content.component.ts
@@ -9,7 +9,7 @@ import { deprecatedSelector } from '../deprecated-selector';
   <span *ngIf="!template">{{ node.displayField }}</span>
   <ng-container
     [ngTemplateOutlet]="template"
-    [ngOutletContext]="{ $implicit: node, node: node, index: index }">
+    [ngTemplateOutletContext]="{ $implicit: node, node: node, index: index }">
   </ng-container>`,
 })
 export class TreeNodeContent {
