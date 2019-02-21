eb8e37180ece6c4e4200395ae73ddf199837b320
Adam Klein <adam@500tech.com>
2019-02-22 00:45:19 +0200

    example build

--- example/cli/dist/styles.js
+++ example/cli/dist/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/adamklein/projects/ng2tree/example/cli/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/adamklein/projects/angular-tree-component/example/cli/src/styles.css */"./src/styles.css");
 
 
 /***/ })
